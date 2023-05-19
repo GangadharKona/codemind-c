043b6f9e01d5668d6291c9f42da9483da82e9f6f
Gangadhar Kona <119723733+GangadharKona@users.noreply.github.com>
2023-05-19 11:06:28 +0530

    #include <stdio.h> int main() { 	int x, y, res; 	scanf("%d %d", &x, &y); 	res = (x > y) ? x : y;
    	while (1) {
    		if (res % x == 0 && res % y == 0) {
    			printf("%d",res);
    			break;
    		}
    		++res;
    	}
    
    	return 0;
    }

--- LCM.c
+++ LCM.c
@@ -1,15 +1,16 @@
-#include<stdio.h>
-int main()
-{
-    int i=0,n,m;
-    scanf("%d%d",&n,&m);
-    while(1)
-    {
-        i++;
-        if(n*i%m==0)
-        {
-            break;
-        }
-    }
-    printf("%d",n*i);
+#include <stdio.h>
+int main() {
+	int x, y, res;
+	scanf("%d %d", &x, &y);
+	res = (x > y) ? x : y;
+
+	while (1) {
+		if (res % x == 0 && res % y == 0) {
+			printf("%d",res);
+			break;
+		}
+		++res;
+	}
+
+	return 0;
 }
