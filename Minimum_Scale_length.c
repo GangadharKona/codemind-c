dbc6422ace6827da476bf09267f28096ef190e35
Gangadhar Kona <119723733+GangadharKona@users.noreply.github.com>
2023-05-24 19:39:28 +0530

    #include<stdio.h> int main() {     int n,i,hcf;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     hcf=arr[0];     int j=1;     while(j<n)     {         if(arr[j]%hcf==0)         {             j++;         }         else         {             hcf=arr[j]%hcf;             i++;         }     }     printf("%d",hcf);     return 0; }

--- Minimum_Scale_length.c
+++ Minimum_Scale_length.c
@@ -1,35 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int n,scales[10],i,j,found=0,min=9999;
+    int n,i,hcf;
     scanf("%d",&n);
+    int arr[n];
     for(i=0;i<n;i++)
     {
-        scanf("%d",&scales[i]);
+        scanf("%d",&arr[i]);
     }
-    for(i=0;i<n;i++)
+    hcf=arr[0];
+    int j=1;
+    while(j<n)
     {
-        if(min>scales[i])
+        if(arr[j]%hcf==0)
         {
-            min=scales[i];
+            j++;
+        }
+        else
+        {
+            hcf=arr[j]%hcf;
+            i++;
         }
     }
-    for(i=min;i>=1;i--)
-    {
-        found=0;
-        for(j=0;j<n;j++)
-        {
-        if(scales[j]%i!=0)
-        {
-            found=1;
-            break;
-        }
-    }
-    if(found==0)
-    {
-        printf("%d",i);
-        break;
-    }
-}
-return 0;
+    printf("%d",hcf);
+    return 0;
 }
